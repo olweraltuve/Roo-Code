1111342fbacfa3458a67fbaf6a49a80feef09022
cte <cestreich@gmail.com>
2025-03-05 09:38:13 -0800

    Remove :dark prefix

--- webview-ui/src/components/settings/SectionHeader.tsx
+++ webview-ui/src/components/settings/SectionHeader.tsx
@@ -8,9 +8,7 @@ type SectionHeaderProps = HTMLAttributes<HTMLDivElement> & {
 }
 
 export const SectionHeader = ({ description, children, className, ...props }: SectionHeaderProps) => (
-	<div
-		className={cn("sticky top-0 z-10 bg-vscode-sideBar-background dark:brightness-90 px-5 py-4", className)}
-		{...props}>
+	<div className={cn("sticky top-0 z-10 bg-vscode-sideBar-background brightness-90 px-5 py-4", className)} {...props}>
 		<h4 className="m-0">{children}</h4>
 		{description && <p className="text-vscode-descriptionForeground text-sm mt-2 mb-0">{description}</p>}
 	</div>
